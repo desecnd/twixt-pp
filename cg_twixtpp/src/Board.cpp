0b9e2d12952977b0e9a57cb201f7adba160ef6b7
pavveu <pawel@ewnet.pl>
2020-05-31 23:16:28 +0200

    Updated Board class

--- cg_twixtpp/src/Board.cpp
+++ cg_twixtpp/src/Board.cpp
@@ -29,28 +29,28 @@ void Board::dfs(std::vector<std::vector<int>>& vis, Position pos, int player) {
 }
 
 bool Board::isGameOver() {
-    std::vector<std::vector<int>> vis(rows_, std::vector<int>(cols_, 0));
+    std::vector<std::vector<int>> vis(kRows, std::vector<int>(kCols, 0));
 
-    for (int col = 0; col < cols_; col++)
+    for (int col = 0; col < kCols; col++)
         if ( pegOwner(Position(0, col)) == 1 ) 
             dfs(vis, Position(0, col), 1);
 
-    for (int col = 0; col < cols_; col++)
-        if  ( vis[rows_ - 1][col] ) return true;
+    for (int col = 0; col < kCols; col++)
+        if  ( vis[kRows - 1][col] ) return true;
 
-    for (int row = 0; row < rows_; row++)
+    for (int row = 0; row < kRows; row++)
         if ( pegOwner(Position(row, 0)) == 2 ) 
             dfs(vis, Position(row, 0), 2);
 
-    for (int row = 0; row < rows_; row++)
-        if  ( vis[row][cols_ - 1] ) return true;
+    for (int row = 0; row < kRows; row++)
+        if  ( vis[row][kCols - 1] ) return true;
 
     return false;
 }
 
 void Board::debug() {
-    for (int r = 0; r < rows_; r++) {
-        for (int c = 0; c < cols_; c++) {
+    for (int r = 0; r < kRows; r++) {
+        for (int c = 0; c < kCols; c++) {
             std::cerr << pegOwner(Position(r, c)) << " ";
             // std::cerr << index(Position(r, c)) << " ";
         }
--- cg_twixtpp/src/Board.hpp
+++ cg_twixtpp/src/Board.hpp
@@ -9,24 +9,27 @@
 // player 1 or 2 
 // position taken on empty
 
+
 class Board {
-private: 
-    static constexpr int rows_ { 12 };
-    static constexpr int cols_ { 12 };
+public:
+    static constexpr int kRows { 12 };
+    static constexpr int kCols { 12 };
     static constexpr CollisionTable collisionTable_{};
+private:
     int currentPlayer_ { 1 };
-    std::array<std::uint8_t, rows_ * cols_> pegs_ {};
-    std::array<std::uint16_t, rows_ * cols_> links_ {};
+    std::array<std::uint8_t, kRows * kCols> pegs_ {};
+    std::array<std::uint16_t, kRows * kCols> links_ {};
+
 
     int index(Position pos) const { 
-        return pos.row() * cols_ + pos.col(); 
+        return pos.row() * kCols + pos.col(); 
     }
     int opponent(int player) const { 
         return 3 - player; 
     }
 
     bool valid(Position pos) const { 
-        return (pos.row() >= 0 && pos.row() < rows_ && pos.col() >= 0 && pos.col() < cols_ );
+        return (pos.row() >= 0 && pos.row() < kRows && pos.col() >= 0 && pos.col() < kCols );
     }
 
     void markPeg(Position pos, int player) { 
@@ -37,16 +40,13 @@ private:
         links_[index(pos)] |= (1 << (DIRECTIONS * (player - 1) + static_cast<int>(dir)));
         links_[index(pos + getVector(dir))] |= (1 << (DIRECTIONS * (player - 1) + static_cast<int>(opposite(dir))));
 
-        std::cerr << "Created link: " << pos << " -> " << static_cast<int>(dir) << ", for player" << player << "\n";
-    }
-
-    bool linkExist(Position pos, Direction dir, int player) const {
-        return (links_[index(pos)] & ( 1<<(DIRECTIONS * player + static_cast<int>(dir)) ));
+        // std::cerr << "{Created link: " << pos << " -> " << static_cast<int>(dir) << ", for player" << player << "}";
+        // std::cerr << " == {Created link: " << (pos + getVector(dir)) << " -> " << static_cast<int>(opposite(dir)) << ", for player" << player << "}";
     }
 
     bool linkPossible(Position pos, Direction direction, int player) const {
         for ( const Link& link : collisionTable_.collisions(direction) ) {
-            if ( linkExist(link.position, link.direction, opponent(player)) ) 
+            if ( valid(pos + link.position) && linkExist(pos + link.position, link.direction, opponent(player)) ) 
                 return false;
         }
         return true;
@@ -55,6 +55,10 @@ private:
     void dfs(std::vector<std::vector<int>>& vis, Position pos, int player);
 public:
     Board() = default;
+
+    bool linkExist(Position pos, Direction dir, int player) const {
+        return (links_[index(pos)] & ( 1<<(DIRECTIONS * (player - 1) + static_cast<int>(dir)) ));
+    }
 
     int pegOwner(Position pos) const {
         if ( pegs_[index(pos)] & 2 ) return 2;
@@ -66,5 +70,6 @@ public:
     bool isGameOver();
     void debug();
 };
+
 
 extern constexpr CollisionTable Board::collisionTable_;
