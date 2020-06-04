45c85558f5255328fdc3858a6eb39615ac4c1cfc
pavveu <pawel@ewnet.pl>
2020-06-04 18:07:02 +0200

    Moved board debug into human controler

--- cg_twixtpp/src/Judge.cpp
+++ cg_twixtpp/src/Judge.cpp
@@ -5,27 +5,26 @@ int Judge::playGame(Player& p1, Player& p2, bool debug = false) {
     Move m1 { "FIRST" };
     Move m2 { "FIRST" };
 
+    p1.setPerspective(1);
+    p2.setPerspective(2);
+
+    std::cerr << "Kappa" << std::endl;
+
     int roundsLeft { 12 * 12 - 4 };
-
-    agent::BoardEvaluator be{};
-
-    if ( debug ) { printBoard(std::cout, board_); std::cerr << std::endl; }
     while ( roundsLeft > 0 ) {
-        do { m1 = p1.getMove(board_); } while(false);
-        applyMove(board_, m1);
-
-        if ( debug ) { printBoard(std::cout, board_); std::cerr << std::endl; }
+        do { 
+            m1 = p1.getMove(board_); 
+        } while( !applyMove(board_, m1) );
 
         if ( board_.isGameOver() ) return 1;
 
-        do { m2 = p2.getMove(board_); } while(false);
-        applyMove(board_, m2);
+        do { 
+            m2 = p2.getMove(board_); 
+        } while( !applyMove(board_, m2) );
 
-        if ( debug ) { printBoard(std::cout, board_); std::cerr << std::endl; }
         if ( board_.isGameOver() ) return 2;
 
         roundsLeft -= 2;
     }
-    if ( debug ) { printBoard(std::cout, board_); std::cerr << std::endl; }
     return 0;
 }
